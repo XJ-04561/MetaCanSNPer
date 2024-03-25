1424fb00c08e9ae93c5e6271e500a3444f422782
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-03-25 15:15:02 +0100

    Fix

--- MetaCanSNPer/modules/Databases.py
+++ MetaCanSNPer/modules/Databases.py
@@ -90,7 +90,10 @@ class DatabaseReader:
 		if genome is None:
 			return self._connection.execute(f"SELECT {SNP_COLUMN_SNP_ID} FROM {TABLE_NAME_SNP_ANNOTATION} WHERE {SNP_COLUMN_POSITION} = ?", [pos]).fetchall()
 		else:
-			return self._connection.execute(f"SELECT {SNP_COLUMN_SNP_ID} FROM {TABLE_NAME_SNP_ANNOTATION} WHERE {SNP_COLUMN_POSITION} = ? AND {SNP_COLUMN_GENOME_ID} = ?", [pos, *self.genomeID(genome)]).fetchone()
+			if (out := self._connection.execute(f"SELECT {SNP_COLUMN_SNP_ID} FROM {TABLE_NAME_SNP_ANNOTATION} WHERE {SNP_COLUMN_POSITION} = ? AND {SNP_COLUMN_GENOME_ID} = ?", [pos, *self.genomeID(genome)]).fetchone()) is not None:
+				return out[0]
+			else:
+				raise ValueError(f"No SNP found at {pos=} in {genome=}")
 
 	@cached_property
 	def SNPsByID(self) -> dict[str,Iterable[tuple[str,int,str,str]]]:
