b973cadf358b253c64c744a78bd081ac3fd8a66d
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-04-02 21:42:15 +0200

    Fix

--- MetaCanSNPer/modules/__init__.py
+++ MetaCanSNPer/modules/__init__.py
@@ -2,5 +2,4 @@ from MetaCanSNPer.modules.MetaCanSNPer import MetaCanSNPer
 import MetaCanSNPer.modules.Aligners as Aligners
 import MetaCanSNPer.modules.Mappers as Mappers
 import MetaCanSNPer.modules.SNPCallers as SNPCallers
-from MetaCanSNPer.modules.Databases import DatabaseReader
 import MetaCanSNPer.modules.DownloadReferences as DownloadReferences
