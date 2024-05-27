71372e0b2690122401e1041ca64ef521c8c97d8e
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-05-27 14:44:46 +0200

    Update TerminalUpdater.py

--- MetaCanSNPer/core/TerminalUpdater.py
+++ MetaCanSNPer/core/TerminalUpdater.py
@@ -277,14 +277,14 @@ class Indicator(Logged):
 			self.condition.acquire(timeout=0.40)
 		
 		with self.rowLock:
-			flushPrint.clear()
 			if None in self.threads.values():
-				flushPrint(self.rowTemplate.format(time=f"{red('Failed!')} {formatTimestamp(timer()-startTime)}\n\r", names=self.shortKeys, bars=tuple(self.rowGenerator)))
+				flushPrint((self.rowTemplate+"\n\r").format(time=f"{red('Failed!')} {formatTimestamp(timer()-startTime)}", names=self.shortKeys, bars=tuple(self.rowGenerator)))
 			elif self.finishedThreads.issuperset(self.threads):
+				flushPrint.clear()
 				flushPrint(f"{self.message} {green('Done!')} {formatTimestamp(timer()-startTime)}")
-				flushPrint("\n\r")
+				print("\n\r", flush=True, file=self.out)
 			else:
-				flushPrint(self.rowTemplate.format(time=f"{yellow('Interrupted!')} {formatTimestamp(timer()-startTime)}\n\r", names=self.shortKeys, bars=tuple(self.rowGenerator)))
+				flushPrint((self.rowTemplate+"\n\r").format(time=f"{yellow('Interrupted!')} {formatTimestamp(timer()-startTime)}", names=self.shortKeys, bars=tuple(self.rowGenerator)))
 	
 	def checkDone(self):
 		if all(v is None or v == 2 or v == 3 for v in self.threads.values()):
