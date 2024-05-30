79c7fd99c6466842b7d73e8c2dc4baf2adb3bffa
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-05-30 23:48:46 +0200

    Update __init__.py

--- MetaCanSNPer/__init__.py
+++ MetaCanSNPer/__init__.py
@@ -4,6 +4,6 @@ __doc__ = "Meta-Canonical SNP analyzer. An aggregate program that neatly outputs
 from MetaCanSNPer.core.MetaCanSNPer import MetaCanSNPer
 from MetaCanSNPer.core.Hooks import Hooks, Hook, DummyHooks
 from MetaCanSNPer.modules.Database import MetaCanSNPerDatabase, ReferencesTable, ChromosomesTable, SNPsTable, TreeTable
-from MetaCanSNPer.modules.Downloader import ReferenceDownloader, DatabaseDownloader, DatabaseThread
+from MetaCanSNPer.modules.Downloader import ReferenceDownloader, DatabaseDownloader, ThreadConnection
 from MetaCanSNPer.core.TerminalUpdater import TerminalUpdater, Spinner, LoadingBar, TextProgress
 from MetaCanSNPer.core.Commands import Command
