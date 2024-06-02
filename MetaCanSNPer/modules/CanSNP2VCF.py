7315ca8a6b41cfcb993505dda5e0d8fee56d8100
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-06-03 01:11:35 +0200

    Update CanSNP2VCF.py

--- MetaCanSNPer/modules/CanSNP2VCF.py
+++ MetaCanSNPer/modules/CanSNP2VCF.py
@@ -1,8 +1,9 @@
 import os
 
 def CanSNP2VCF(lib : "DirectoryLibrary", force : bool=False):
-	from MetaCanSNPer.modules.Database import Chromosome, Position, GenomeID
-	from VariantCallFixer import openVCF
+	from MetaCanSNPer.modules.Database import Chromosome, Position, GenomeID, ChromosomesTable
+	from MetaCanSNPer.Globals import SOFTWARE_NAME
+	from VariantCallFixer import openVCF, Reference, Source, Contig
 	for genomeID, genome, refPath, SNPPath in map(lambda x: (x[0], x[1], lib.references[x[1]], lib.targetSNPs[x[1]]), lib.database.references):
 		if SNPPath is not None and not force:
 			continue
@@ -16,11 +17,15 @@ def CanSNP2VCF(lib : "DirectoryLibrary", force : bool=False):
 			raise FileNotFoundError(f"Could not find file for reference genome {genome!r}. Either the file has not been downloaded or the path to the file is not correct. Path to file was {refPath!r}")
 
 		filePath = path / filename
-		with openVCF(filePath, mode="w", referenceFile=refPath, chrom=genome) as vcfFile:
-			lib.database.LOG.info(repr(lib.database[Position, Chromosome, GenomeID == genomeID, +Position]))
+		with openVCF(filePath, mode="w") as vcfFile:
+			vcfFile.addMeta(Source(SOFTWARE_NAME))
+			vcfFile.addMeta(Reference(refPath))
+			for chromosome in lib.database[Chromosome, ChromosomesTable, GenomeID == genomeID]:
+				vcfFile.addMeta(Contig(ID=chromosome, URL=refPath))
+			
 			for position, chromosome in lib.database[Position, Chromosome, GenomeID == genomeID, +Position]:
-				vcfFile.add(CHROM=chromosome, POS=position, REF="N", ALT="A,T,C,G")
-			lib.targetSNPs[genome] = filePath
+				vcfFile.add(CHROM=chromosome, POS=position, REF="N", ALT=("A", "T", "C", "G"))
+		lib.targetSNPs[genome] = filePath
 
 try:
 	from MetaCanSNPer.core.DirectoryLibrary import DirectoryLibrary
