608fe5530e9c71500d3c29300a4314c756955a08
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-06-03 23:21:16 +0200

    Remove Python >3.10 sepcifics

--- MetaCanSNPer/Globals.py
+++ MetaCanSNPer/Globals.py
@@ -36,7 +36,11 @@ from functools import cache, cached_property
 from typing import Iterable, Callable, Any, Generator, Literal, AnyStr, TextIO, BinaryIO, Self, overload
 from time import sleep
 from collections import defaultdict, OrderedDict
-import tomllib as toml
+if sys.version_info < (3, 11):
+	import toml
+else:
+	import tomllib as toml
+	
 from appdirs import user_log_dir, user_config_dir, site_config_dir
 from threading import Lock
 
