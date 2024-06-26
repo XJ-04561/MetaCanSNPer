8a14f12846371d4c040ea374c6c7144ba1cee99c
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-06-26 15:42:14 +0200

    Update MetaCanSNPer.py

--- MetaCanSNPer/core/MetaCanSNPer.py
+++ MetaCanSNPer/core/MetaCanSNPer.py
@@ -279,8 +279,8 @@ class MetaCanSNPer(Logged):
 			vcfFile = openVCF(filePath, "r")
 			for chrom, pos, ref, alt, counts in vcfFile["CHROM", "POS", "REF", "ALT", "AD"]:
 				depth = sum(counts)
+				LOG.debug(f"(CHROM, POS, Allele-readDepth (A, T, C, G) ) {chrom}, {pos}, {depth} = {' + '.join(map(str, counts))}, {ref=}, {alt=}")
 				counts = tuple(counts[''.join([ref, *alt]).find(base)] for base in "ATCG")
-				LOG.debug(f"(CHROM, POS, Allele-readDepth (A, T, C, G) ) {chrom}, {pos}, {depth} = {' + '.join(map(str, counts))}")
 				
 				chromID = next(self.database[ChromosomeID, Chromosome==chrom, Genome==genome], None)
 				nodeID, derivedBase, ancestralBase = self.database[NodeID, DerivedBase, AncestralBase, Position==pos, ChromosomeID==chromID]
