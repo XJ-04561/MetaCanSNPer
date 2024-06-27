e8b53136bfa5ebe54a8649a5c076b9f4c64a23c5
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-06-27 13:24:58 +0200

    Update test_script.py

--- tests/test_script.py
+++ tests/test_script.py
@@ -11,7 +11,21 @@ command = (
 N = 25
 BYTES = 2 * (10 **9)
 
+BANNED = [
+	("FSC148", round(BYTES*1.0)),
+	("SCHUS4", round(BYTES*1.0)),
+	("LVS", round(BYTES*0.8)),
+	("FSC148", round(BYTES*0.8)),
+	("SCHUS4", round(BYTES*0.8)),
+	("FSC148", round(BYTES*0.6)),
+	("SCHUS4", round(BYTES*0.6)),
+	("FSC148", round(BYTES*0.4)),
+]
+
 for f in [1.0, 0.8, 0.6, 0.4, 0.2, 0.1, 0.05, 0.025, 0.01]:
 	for name in names:
-		print(f"Running {name} {N} times, at {round(100*f, 1)}% of {BYTES:.0e} ({round(BYTES*f)} bytes)")
-		os.system(command.format(n=N, name=name, size=round(BYTES*f)))
+		if (name, round(BYTES*f)) not in BANNED:
+			print(f"Running {name} {N} times, at {round(100*f, 1)}% of {BYTES:.0e} ({round(BYTES*f)} bytes)")
+			os.system(command.format(n=N, name=name, size=round(BYTES*f)))
+		else:
+			print(f"Skipping {name} {N} times, at {round(100*f, 1)}% of {BYTES:.0e} ({round(BYTES*f)} bytes)")
