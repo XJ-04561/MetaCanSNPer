d10bad14f583ee9b87cdc240d6362576ae1efd71
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-07-05 16:52:16 +0200

    Update SubSampling.py

--- MetaCanSNPer/modules/SubSampling.py
+++ MetaCanSNPer/modules/SubSampling.py
@@ -122,12 +122,12 @@ def splitFastq(files : int, source : FilePath|FileList[FilePath], *,
 						lineSep = line
 						break
 					read.append(line)
-				readLength = sum(map(len, map(bytes.strip, read)))
+				
 				if lineSep.strip() == b"+":
-					file.seek(1, readLength)
-				else:
-					file.seek(1, -len(lineSep))
-				readList.append([readLength, pos, file.tell()])
+					file.seek(sum(map(len, read)), 1)
+				elif lineSep:
+					file.seek(-len(lineSep), 1)
+				readList.append([sum(map(len, map(bytes.strip, read))), pos, file.tell()])
 
 			readsIndex.append(readList)
 	LOGGER.info(f"Sub Sampling: Found {', '.join(map(len, readsIndex))} reads for the source file(s).")
